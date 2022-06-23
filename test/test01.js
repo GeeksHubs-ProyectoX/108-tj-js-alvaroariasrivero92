e6e340afe4928b197af3cfc5be51e9fb475e5096
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-06-23 15:55:46 +0200

    tests passed

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,9 @@
 function toMilliseconds(h,m,s){
-  return null;
+  if((h<0 || h>23) || (m<0 || m>59) || (s<0 || s>59)){
+    return -1
+  }else{
+    return h*3600000+m*60000+s*1000
+  }
 }
 
 module.exports = toMilliseconds;
